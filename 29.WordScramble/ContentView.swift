47ec7e410f1ca21d0479d5572632349c56661c2c
Valentin <pamerra@mail.ru>
2025-09-01 19:56:40 +0300

    75.6/7.Адаптировал для проговаривания с экрана

--- 29.WordScramble/ContentView.swift
+++ 29.WordScramble/ContentView.swift
@@ -35,6 +35,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityLabel("\(word.count) letters ")
                     }
                 }
             }
